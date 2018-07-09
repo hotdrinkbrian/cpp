0cf0361b8a7f51cfddf7c376b3dfa86c0ef930da
brian <hotdrink1104@gmail.com>
2018-07-10 01:21:39 +0200

    s

--- h.h
+++ h.h
@@ -5,17 +5,41 @@
 #include<iostream>
 
 using namespace std;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~namespace
+namespace chinese{
+    string LCW(){ return "Li Zong Wei";}
+}
+namespace english{
+    string LCW(){ return "Lee Chong Wei";}
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~namespace
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~template
+template < class B >
+class player
+{
+    private:
+        B height;
+    public:
+        void setHeight( B pp ){ height = pp; };
+        B getHeight(){ return height; };
+};
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~template
 
 class Racket
 {
     public:
         int length;
 	    int year;
+        static int NumOfRac;
         void setWeight(int pp);
         int getWeight();
+        int getNumRac(){ return NumOfRac; };
     private:
 	    int weight;	
 };
+
+int Racket::NumOfRac = 0;
 
 class yonex : public Racket
 {
@@ -24,26 +48,22 @@ class yonex : public Racket
 
     public:
 	    int getYear();
+        string getArea();
 	    void setYear(int pp);
+        void setArea(string pp);
 	    yonex();
 	    ~yonex();
 };
 
 
 
-void Racket::setWeight(int pp)
-{
-    weight = pp;
-}
-
-int Racket::getWeight()
-{
-    return weight;
-}
+void Racket::setWeight(int pp){ weight = pp; }
+int Racket::getWeight(){ return weight; }
 
 yonex::yonex()
 {
     year = 2018;
+    NumOfRac++;
     cout << "yy created!" << endl;
 }
 
@@ -52,16 +72,11 @@ yonex::~yonex()
     cout << "yy deleted!" << endl;
 }
 
-int yonex::getYear()
-{
-    return year;
-}
+int yonex::getYear(){ return year; }
+string yonex::getArea(){ return area; }
 
-void yonex::setYear(int pp)
-{
-    year = pp;
-}
-
+void yonex::setYear(int pp){ year = pp; }
+void yonex::setArea(string pp){ area = pp; }
 
 
 
--- t.cpp
+++ t.cpp
@@ -1,22 +1,41 @@
 #include "h.h" 
 
+//int Racket::NumOfRac = 0;
+
 int main()
 {
-    //Racket vt80;
+    //namespace
+    using namespace english;
+    cout << chinese::LCW() << endl;
+    cout << LCW() << endl;
+
+    //class
     yonex vt80;
     vt80.length = 675;
     vt80.setWeight(85);
-    //vt80.weight = 85;
     vt80.setYear(2011);
+    vt80.setArea("Japan");
     cout << vt80.length << endl;
     cout << vt80.getWeight() << endl; 
-    //cout << vt80.getYear() << endl; 
+    cout << vt80.getArea() << endl;
+    cout << vt80.getNumRac() << endl;
 
+    //pointer
     yonex *vt70 = new yonex;
     yonex *vt80_pt;
     vt80_pt = &vt80;
     cout << vt70->getYear() << endl; 
     cout << vt80_pt->getYear() << endl; 
+    cout << vt80.getNumRac() << endl;
+
+    //template
+    player<int> LD;
+    LD.setHeight(178);
+    cout << LD.getHeight() << endl;
+    player<string> MMT;
+    MMT.setHeight("173");
+    cout << MMT.getHeight() << endl;
+
 
     delete vt70;
     cout << vt80.getYear() << endl;
